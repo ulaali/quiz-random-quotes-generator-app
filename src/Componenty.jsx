8fef180f01634dedcce4632f38983c4456bac88f
ulaali <ulaali513@yahoo.com>
2023-07-31 12:05:45 +0300

    random quots generator

--- src/Componenty.jsx
+++ src/Componenty.jsx
@@ -1,31 +1,27 @@
 import { useState } from "react";
 
 export default function Componenty() {
-  let [right, setRight] = useState();
-  let options = [
-    { answer: "Apple", statee: false },
-    { answer: "Bananas", statee: true },
-    { answer: "none of the above", statee: false },
+  let quots = [
+    "joey doesnt share food",
+    "boyfriend and girlfriend are gonna come and go but friendship is for life",
+    "you will never know unless you walk in my shoes",
+    "cream soda (google it)",
+    "i wil never worry about nothing",
   ];
+  let [quot, setQuot] = useState(quots[0]);
+
   return (
-    <div className="quiz">
-      <h1>Humans share more than 50% of their DNS with which fruit?!</h1>
-      <div>
-        {options.map((i) => {
-          return(
-            <button onClick={()=>setRight(i.statee)} className="btn">
-            <p> {i.answer}</p> 
-              <div style={{ backgroundColor: "blue",display:"flex",justifyContent:"center" , alignItems:"center", marginLeft:"330px",fontSize:"1.2rem"}}>
-                { right && right === i.statee ? "that's correct!" : ""}
-              </div>
-            </button>
-          );
+    <div className="generator">
+      <h1>Click the button to randomly generate quots</h1>
 
-        }
-        )
-        }
-      </div>
-
+      <button
+        onClick={() => {
+          let qq = Math.floor(Math.random() * quots.length);
+          setQuot(quots[qq]);
+        }}
+      >
+        {quot}
+      </button>
     </div>
   );
 }
